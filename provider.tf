8b2112f9b5f13937e5b0ed810668a13a765f3e99
Ragu <vragu84@gmail.com>
2023-08-20 02:14:18 +0530

    VPV files

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.1"
+}
